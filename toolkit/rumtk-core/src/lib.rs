13ccdf5319127a5a33430af43182dc48432402fc
Luis Miguel Santos <kiseitai2@yahoo.com>
2024-05-27 18:00:54 -0400

    Setting up a few basic unit tests to begin planning out which data types I need to define and the desired API.

--- toolkit/rumtk-core/src/lib.rs
+++ toolkit/rumtk-core/src/lib.rs
@@ -1,14 +1,72 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+mod net;
+mod log;
+mod hl7_v2;
+mod hl7_fhir;
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_server_listen() {
+        let server = net::TCPSever::new();
+        server.start();
+        //assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn test_client_send() {
+        let test_str = String::from("Hello World!");
+        let server = net::TCPSever::new();
+        let client = net::TCPClient::new();
+        let port = 55555;
+        server.start(port);
+        client.connect(port);
+        client.send(&test_str.as_bytes());
+        let result = String::from(server.pop());
+        assert_eq!(result, test_str);
+    }
+
+    #[test]
+    fn test_log_to_file() {
+        let logger_name = String::from("test_logger");
+        let logger_path = String::from("logs");
+        let logger = log::new_logger(logger_path, logger_name, log::LOGLEVEL::INFO);
+        let test_str = String::from("Hello World!");
+        log::log_info(test_str);
+        assert_eq!(result, test_str);
+    }
+
+    #[test]
+    fn test_load_hl7_v2_message() {
+        let message = hl7_v2::new(tests::DEFAULT_HL7_V2_MESSAGE);
+        let message_segment = String::from("MSH");
+        let test_str = String::from("");
+        assert_eq!(message.get(message_segment), test_str);
+    }
+
+    #[test]
+    fn test_load_hl7_v2_message_segment() {
+        let message = hl7_v2::new(tests::DEFAULT_HL7_V2_MESSAGE);
+        let message_segment = String::from("MSH");
+        let test_str = String::from("");
+        assert_eq!(message.get(message_segment), test_str);
+    }
+
+    #[test]
+    fn test_load_hl7_v2_message_segment_field() {
+        let message = hl7_v2::new(tests::DEFAULT_HL7_V2_MESSAGE);
+        let message_segment = String::from("MSH-1");
+        let test_str = String::from("");
+        assert_eq!(message.get(message_segment), test_str);
+    }
+
+    #[test]
+    fn test_load_hl7_v2_message_segment_field_node() {
+        let message = hl7_v2::new(tests::DEFAULT_HL7_V2_MESSAGE);
+        let message_segment = String::from("PID-3.2");
+        let test_str = String::from("");
+        assert_eq!(message.get(message_segment), test_str);
     }
 }
