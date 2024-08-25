8d03cc667337fe15dcd1cbef0fab09de1bd36af0
Luis Miguel Santos <kiseitai2@yahoo.com>
2024-08-24 20:39:59 -0400

    Making sure to add the proper copyright notices I have been too lazy to set up in IDE until today...

--- toolkit/rumtk-core/src/maths.rs
+++ toolkit/rumtk-core/src/maths.rs
@@ -1,3 +1,24 @@
+/*
+ * rumtk attempts to implement HL7 and medical protocols for interoperability in medicine.
+ * This toolkit aims to be reliable, simple, performant, and standards compliant.
+ * Copyright (C) 2024  Luis M. Santos, M.D.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+
 use std::ops::{Range};
 
 const base_ten: u8= 10;
@@ -7,5 +28,5 @@ pub fn generate_tenth_factor(tenth_place: u32) -> u32 {
     for _ in irange {
         factor *= base_ten as u32;
     }
-    return factor;
+    factor
 }
