db3330d87cb2d7c4b367908b03e216a4e0dec707
kiseitai3 <kiseitai3@gmail.com>
2025-05-07 21:25:32 -0400

    Silencing blocks that were getting picked up as doctests.

--- toolkit/rumtk-hl7-v2/src/hl7_v2_base_types.rs
+++ toolkit/rumtk-hl7-v2/src/hl7_v2_base_types.rs
@@ -25,13 +25,13 @@ pub mod v2_base_types {
     };
     use crate::hl7_v2_search::REGEX_V2_SEARCH_DEFAULT;
     use chrono::prelude::*;
+    use rumtk_core::core::RUMResult;
     use rumtk_core::maths::generate_tenth_factor;
     use rumtk_core::search::rumtk_search::{
         string_search, string_search_named_captures, SearchGroups,
     };
     use rumtk_core::strings::{format_compact, StringUtils, ToCompactString};
     use rumtk_core::strings::{RUMString, RUMStringConversions, UTFStringExtensions};
-    use rumtk_core::core::RUMResult;
     use std::fmt::Debug;
 
     /**************************** Constants**************************************/
@@ -110,15 +110,24 @@ pub mod v2_base_types {
 
         pub fn validate_msh_key_chars(msg_key_chars: &str) -> V2Result<&str> {
             if msg_key_chars.len() < 4 {
-                return Err(format_compact!("Too few parser characters! Is MSH malformed? => {}", &msg_key_chars));
+                return Err(format_compact!(
+                    "Too few parser characters! Is MSH malformed? => {}",
+                    &msg_key_chars
+                ));
             }
             if msg_key_chars.len() > 5 {
-                return Err(format_compact!("Too many parser characters! Is MSH malformed? => {}", &msg_key_chars));
+                return Err(format_compact!(
+                    "Too many parser characters! Is MSH malformed? => {}",
+                    &msg_key_chars
+                ));
             }
             if msg_key_chars.is_unique() {
                 return Ok(msg_key_chars);
             }
-            Err(format_compact!("Unknown malformed parser characters! Is MSH malformed? => {}", &msg_key_chars))
+            Err(format_compact!(
+                "Unknown malformed parser characters! Is MSH malformed? => {}",
+                &msg_key_chars
+            ))
         }
 
         fn is_msh(msh_segment_token: &str) -> bool {
@@ -197,6 +206,7 @@ pub mod v2_base_types {
     /// 2A.3.76 ST - string data
     ///
     /// # Definition:
+    /// ```text
     ///     The String data type is used for text data when the appearance of text does not bear
     ///     meaning. This is true for formalized text, symbols and formal expressions, and all kinds of names
     ///     intended for machine processing (e.g., sorting, querying, indexing, etc.).
@@ -209,42 +219,48 @@ pub mod v2_base_types {
     ///     characters. For Unicode this is any code point with a Basic Type of Graphic, except the defined
     ///     escape characters and defined delimiter characters; see The Unicode Standard section 2.4
     ///     <http://www.unicode.org/versions/Unicode10.0.0/ch02.pdf> for details.
-    ///
+    /// ```
     /// ## Example 1:
+    /// ```text
     ///     A textual ST field:
     ///     |almost any data at all|
+    /// ```
     /// ## Example 2:
+    /// ```text
     ///     URL encoded in an ST component:
     ///         ^http://www.pacs.poupon.edu/wado.jsp^
+    /// ```
     /// ## Example 3:
+    /// ```text
     ///     ISO OID encoded in an ST subcomponent:
     ///         &2.16.840.1.113883.1.1&
-    ///
+    /// ```
     /// To include any HL7 delimiter character (except the segment terminator) within a string data field,
     /// use the appropriate HL7 escape sequence (see Section 2.7.1, "Formatting Codes”).
-    ///
+    /// ```text
     ///     Minimum Length: Not specified for the type. May be specified in the context of use. Defaults to 1
     ///     Maximum Length: Not specified for the type. May be specified in the context of use
-    ///
+    /// ```
     /// ST has no inbuilt semantics – these are assigned where the ST is used. In each case where ST is
     /// used, minimum, maximum, and conformance lengths may be specified. Unless specified in the
     /// context of use, values of type ST may not be truncated.
     ///
     /// ## Usage note:
+    /// ```text
     ///     The ST data type is intended for short strings (e.g., less than 1000 characters). For longer
     ///     strings the TX or FT data types should be used (see Sections 2.A.79, “TX - text data” or 2.A.31, “FT -
     ///     formatted text data”).
     ///
     ///     Alternate character set note: ST - string data may also be used to express other character sets. See Section
     ///     2.15.9.18, "Character set," and Section 2.15.9.20, "Alternate character set handling" for details.
-    ///
+    /// ```
     pub type V2ST = V2String;
     ///
     /// It's ambiguous how to handle an ID other than keep it as a string and not really validate it. See Section 2A.3.35
     /// Per Section 2A.3.35
     ///
     /// ## Definition:
-    ///
+    /// ```text
     ///     The value of such a field follows the formatting rules for an ST field except that it is
     ///     drawn from a table of legal values. There shall be an HL7 table number associated with ID data
     ///     types. An example of an ID field is OBR-25 Result Status. This data type should be used only for
@@ -256,7 +272,7 @@ pub mod v2_base_types {
     ///     legal values are taken from code systems defined by other bodies (such as IANA mime types). In
     ///     these cases, a different conformance length may be specified where the ID data type is used. It is
     ///     never acceptable to truncate an ID value.
-    ///
+    /// ```
     pub type V2ID = V2String;
     ///
     /// Pretty much the same as the ID type so we are simply aliasing that type here.
@@ -266,7 +282,7 @@ pub mod v2_base_types {
     /// Per Section 2A.3.36
     ///
     /// ## Definition
-    ///
+    /// ```text
     ///     Minimum Length: ≥ 1- Varies - dependent on length of shortest code in code set.
     ///     Maximum Length: Varies - dependent on length of longest code in code set.
     ///
@@ -285,7 +301,7 @@ pub mod v2_base_types {
     ///     type for user-defined tables.
     ///
     ///     It is never acceptable to truncate an IS value.
-    ///
+    /// ```
     pub type V2IS = V2ST;
     ///
     /// Formatted Text type
@@ -295,29 +311,31 @@ pub mod v2_base_types {
     /// Per Section 2A.3.31
     ///
     /// # Definition:
-    ///
+    /// ```text
     ///     This data type is derived from the TX data type by allowing the addition of embedded
     ///     formatting instructions. These instructions are limited to those that are intrinsic and independent of
     ///     the circumstances under which the field is being used. The actual instructions and their
     ///     representation are described in section 2.7.6, “Usage and Examples of Formatted Text”. The FT
     ///     field is of arbitrary length (up to 64k) and may contain formatting commands enclosed in escape
     ///     characters.
-    ///
+    /// ```
     pub type V2FT = V2String;
     ///
     /// 2A.3.80 TX - text data
     ///
     /// # Definition:
+    /// ```text
     ///     String data meant for user display (on a terminal or printer). Such data would not
     ///     necessarily be left justified since leading spaces may contribute greatly to the clarity of the
     ///     presentation to the user. Because this type of data is intended for display, it may contain certain
     ///     escape character sequences designed to control the display. Escape sequence formatting is defined
     ///     in Section 2.7, "Use of escape sequences in text fields". Leading spaces should be included.
     ///     Trailing spaces should be removed.
-    ///
+    /// ```
     /// ## Example:
+    /// ```text
     ///  | leading spaces are allowed.|
-    //
+    /// ```
     /// Since TX data is intended for display purposes, the repeat delimiter, when used with a TX data
     /// field, implies a series of repeating lines to be displayed on a printer or terminal. Therefore, the
     /// repeat delimiters are regarded as paragraph terminators or hard carriage returns (e.g., they would
@@ -343,7 +361,7 @@ pub mod v2_base_types {
     /// Per Section 2A.3.22 DTM - date/time
     ///
     /// ## Definition:
-    ///
+    /// ```text
     ///     Specifies a point in time using a 24-hour clock notation.
     ///
     ///     Minimum Length: 4
@@ -351,12 +369,13 @@ pub mod v2_base_types {
     ///
     ///     The number of characters populated (excluding the time zone specification) specifies the
     ///     precision.
-    ///
+    /// ```
     /// ## Format:
+    /// ```text
     ///     YYYY[MM[DD[HH[MM[SS[.S[S[S[S]]]]]]]]][+/-ZZZZ].
-    ///
+    /// ```
     /// ## Thus:
-    ///
+    /// ```text
     ///     a) only the first four are used to specify a precision of "year"
     ///     b) the first six are used to specify a precision of "month"
     ///     c) the first eight are used to specify a precision of "day"
@@ -365,18 +384,18 @@ pub mod v2_base_types {
     ///     f) the first fourteen are used to specify a precision of "second”
     ///     g) the first sixteen are used to specify a precision of "one tenth of a second”
     ///     h) the first nineteen are used to specify a precision of " one ten thousandths of a second”
-    ///
+    /// ```
     /// ## Example:
-    ///
+    /// ```text
     ///     |199904| specifies April 1999.
-    ///
+    /// ```
     /// The time zone (+/-ZZZZ) is represented as +/-HHMM offset from Coordinated Universal Time (UTC)
-    ///
+    /// ```text
     ///     •For implementations prior to V2.9 +0000 or -0000 both represent UTC (without offset).
     ///     •For implementations starting with V2.9
     ///         + use of the plus sign (+0000) represents the civil time zone offset is known to be zero,
     ///         + use of the minus sign (-0000) represents UTC (without offset)
-    ///
+    /// ```
     pub struct V2DateTime {
         year: u16,
         month: u8,
@@ -596,16 +615,17 @@ pub mod v2_base_types {
     /// YYYY\[MM\[DD\]\].
     ///
     /// # Thus:
-    ///
-    ///     a) only the first four digits are used to specify a precision of "year"
+    /// ```text
+    ///     a) only the first four digits are used to specify a precision of 'year
     ///     b) the first six are used to specify a precision of "month"
     ///     c) the first eight are used to specify a precision of "day"
+    /// ```
     ///
     /// # Examples:
-    ///
+    /// ```text
     ///     |19880704|
     ///     |199503|
-    ///
+    /// ```
     /// The DT data type does not follow the normal truncation pattern, and the truncation character is
     /// never valid in the DT data type. Instead, the truncation behavior is based on the semantics of dates.
     ///
@@ -632,12 +652,13 @@ pub mod v2_base_types {
     ///
     /// Format: HH\[MM\[SS\[.S\[S\[S\[S\]\]\]\]\]\]\[+/-ZZZZ\]
     /// Thus:
+    /// ```text
     ///     a) the first two are used to specify a precision of "hour”
     ///     b) the first four are used to specify a precision of "minute”
     ///     c) the first six are used to specify a precision of "second”
     ///     d) the first eight are used to specify a precision of "one tenth of a second”
     ///     e) the first eleven are used to specify a precision of " one ten thousandths of a second”
-    ///
+    /// ```
     /// Example: |0630| specifies 6: 30 AM.
     ///
     /// The fractional seconds could be sent by a transmitter who requires greater precision than whole
@@ -678,20 +699,20 @@ pub mod v2_base_types {
     /// 2A.3.47 NM - numeric
     ///
     /// # Definition:
-    ///
+    /// ```text
     ///     A number represented as a series of ASCII numeric characters consisting of an optional
     ///     leading sign (+ or -), the digits and an optional decimal point. In the absence of a sign, the number
     ///     is assumed to be positive. If there is no decimal point the number is assumed to be an integer.
     ///
     ///     Minimum Length: 1
     ///     Maximum Length: 16
-    ///
+    /// ```
     /// ## Examples:
-    ///
+    /// ```text
     ///     |999|
     ///     |-123.792|
     ///     |0.1|
-    ///
+    /// ```
     /// Values of this data type shall contain at least one digit to the left of the decimal point. This means
     /// that 0.1 is a valid representation, while .1 is not. Leading zeros, or trailing zeros after a decimal
     /// point, are not significant. For example, the following two values with different representations,
@@ -711,33 +732,34 @@ pub mod v2_base_types {
     /// (other than leading zeros).
     ///
     /// ## Example:
-    ///
+    /// ```text
     ///     1.0200 may be truncated to 1.02, but not to 1.0.
-    ///
+    /// ```
     pub type V2NM = f64;
     ///
     /// 2A.3.70 SI - sequence ID
     ///
     /// # Definition:
+    /// ```text
     ///     A non-negative integer in the form of a NM field. The uses of this data type are
     ///     defined in the chapters defining the segments and messages in which it appears.
     ///     Minimum Length: 1
     ///     Maximum Length: 4.
     ///     This allows for a number between 0 and 9999 to be specified.
-    ///
+    ///```
     pub type V2SI = i16;
     ///
     /// 2A.3.72 SNM - string of telephone number digits
     ///
     /// # Definition:
-    ///
+    /// ```text
     ///     A string whose characters are limited to "+" and/or the decimal digits 0 through 9. As
     ///     a string, leading zeros are always considered significant.
     ///     Used only in the XTN data type as of v2.7.
     ///     Minimum Length: 1
     ///     Maximum Length: Not specified for the type. May be specified in the context of use
     ///     SNM is used for telephone numbers, so it is never appropriate to truncate values of type SNM.
-    ///
+    /// ```
     pub type V2SNM = V2String;
 }
 
